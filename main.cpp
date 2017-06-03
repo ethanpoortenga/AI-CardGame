2fdcb252d1a15cf56d70560cda9ba58518547656
Ethan Poortenga <eddp@umich.edu>
2017-06-03 13:41:07 +0200

    some changes

--- main.cpp
+++ main.cpp
@@ -75,135 +75,172 @@ struct deck
     int decksize;
 };
 
-struct gamestatus
+struct gamecontroller
 {
     //FOR THE GAME OVERALL
     int team1score, team2score, whodealt, numplayers;
     //FOR THE HAND
     int bet, whobet;
     string trumpsuit;
-    vector<card> team1cards, team2cards;
+    vector<card> team1cardsplayed, team2cardsplayed;
+    vector<card> team1cardswon, team2cardswon;
     //FOR THE TRICK
     vector<card> trick;
+    int wholeads;
     //FOR RESETTING
     void resettrick()
     {
-        
+        if(findtrickwinner()) team1cardswon.insert(team1cardswon.end(),trick.begin(),trick.end());
+        else team2cardswon.insert(team2cardswon.end(),trick.begin(), trick.end());
+        trick.clear();
     }
     void resethand()
     {
-        
+        int team1pointswon = 0, team2pointswon = 0;
+        if(findhighestcard()) ++team1pointswon;
+        else ++team2pointswon;
+        if(findlowestcard()) ++team1pointswon;
+        else ++team2pointswon;
+        if(findjack()) ++team1pointswon;
+        else ++team2pointswon;
+        if(findgame()) ++team1pointswon;
+        else ++team2pointswon;
+        team1cardswon.clear();
+        team1cardsplayed.clear();
+        team2cardswon.clear();
+        team2cardsplayed.clear();
     }
     void resetgame()
     {
-        
+        team1score = 0;
+        team2score = 0;
+    }
+    bool findtrickwinner() //return true if team1won
+    {
+        return true;
+    }
+    bool findhighestcard() //return true if it's in team1
+    {
+        return true;
+    }
+    bool findlowestcard() //return true if it's in team1
+    {
+        return true;
+    }
+    bool findjack() //return true if it's in team1
+    {
+        return true;
+    }
+    bool findgame() //return true if it's in team1
+    {
+        return true;
     }
 };
 
 class player
 {
 public:
-    virtual void playcard(gamestatus &gamestatus) = 0;
-    virtual bool bet(gamestatus &gamestatus, const int &index) = 0;
-    virtual void forcebet(gamestatus &gamestatus, const int &index) = 0;
+    virtual void playcard(gamecontroller &gamecontroller) = 0;
+    virtual bool bet(gamecontroller &gamecontroller, const int &index) = 0;
+    virtual void forcebet(gamecontroller &gamecontroller, const int &index) = 0;
     vector<card> hand;
 };
 
 class trainer : public player
 {
-    virtual void playcard(gamestatus &gamestatus)
+    virtual void playcard(gamecontroller &gamecontroller)
     {
-        gamestatus.trick.push_back(hand.back());
+        gamecontroller.trick.push_back(hand.back());
         hand.pop_back();
     }
-    bool bet(gamestatus &gamestatus, const int &index)
+    bool bet(gamecontroller &gamecontroller, const int &index)
     {
-        gamestatus.bet = 2;
-        gamestatus.whobet = index;
+        gamecontroller.bet = 2;
+        gamecontroller.whobet = index;
         return true;
     }
-    void forcebet(gamestatus &gamestatus, const int &index)
+    void forcebet(gamecontroller &gamecontroller, const int &index)
     {
-        gamestatus.bet = 2;
-        gamestatus.whobet = index;
+        gamecontroller.bet = 2;
+        gamecontroller.whobet = index;
     }
 };
 
 class learner : public player
 {
-    virtual void playcard(gamestatus &gamestatus)
+    virtual void playcard(gamecontroller &gamecontroller)
     {
-        gamestatus.trick.push_back(hand.back());
+        gamecontroller.trick.push_back(hand.back());
         hand.pop_back();
     }
-    bool bet(gamestatus &gamestatus, const int &index)
+    bool bet(gamecontroller &gamecontroller, const int &index)
     {
-        gamestatus.bet = 2;
-        gamestatus.whobet = index;
+        gamecontroller.bet = 2;
+        gamecontroller.whobet = index;
         return true;
     }
-    void forcebet(gamestatus &gamestatus, const int &index)
+    void forcebet(gamecontroller &gamecontroller, const int &index)
     {
-        gamestatus.bet = 2;
-        gamestatus.whobet = index;
+        gamecontroller.bet = 2;
+        gamecontroller.whobet = index;
     }
 };
 
 class human : public player
 {
-    virtual void playcard(gamestatus &gamestatus)
+    virtual void playcard(gamecontroller &gamecontroller)
     {
         cout << "";
     }
-    bool bet(gamestatus gamestatus, const int &index)
+    bool bet(gamecontroller gamecontroller, const int &index)
     {
         cout << "How much do you want to bet?" << endl;
         int x;
         cin >> x;
         return true;
     }
-    void forcebet(gamestatus gamestatus, const int &index)
+    void forcebet(gamecontroller gamecontroller, const int &index)
     {
-        gamestatus.bet = 2;
-        gamestatus.whobet = index;
+        gamecontroller.bet = 2;
+        gamecontroller.whobet = index;
     }
 };
 
-void setup(vector<player*> &players, deck &carddeck, gamestatus &gamestatus);
+void setup(vector<player*> &players, deck &carddeck, gamecontroller &gamecontroller);
 void dealcards(vector<player*> &players, const deck &carddeck);
-void makebets(const vector<player*> &players, gamestatus &gamestatus);
+void makebets(const vector<player*> &players, gamecontroller &gamecontroller);
 
 int main(int argc, const char * argv[])
 {
     deck carddeck;
     vector<player*> players;
-    gamestatus gamestatus;
-    setup(players, carddeck, gamestatus);
-    while(gamestatus.team1score < 11 && gamestatus.team2score < 11)
+    gamecontroller gamecontroller;
+    setup(players, carddeck, gamecontroller);
+    while(gamecontroller.team1score < 11 && gamecontroller.team2score < 11)
     {
         dealcards(players, carddeck);
-        makebets(players, gamestatus);
+        makebets(players, gamecontroller);
         for(int round = 0; round < 6; ++round)
         {
-            for(int currplayer = gamestatus.whobet, i = 0; i < 4; ++i, currplayer = (currplayer + 1)%gamestatus.numplayers) players[currplayer]->playcard(gamestatus);
-            gamestatus.resettrick();
+            for(int currplayer = gamecontroller.wholeads, i = 0; i < 4; ++i, currplayer = (currplayer + 1)%gamecontroller.numplayers) players[currplayer]->playcard(gamecontroller);
+            gamecontroller.resettrick();
         }
-        gamestatus.resethand();
+        gamecontroller.resethand();
     }
-    if(gamestatus.team1score < 11) cout << "You Lose: Team 2 Wins" << endl;
+    if(gamecontroller.team1score < 11) cout << "You Lose: Team 2 Wins" << endl;
     else cout << "You win!! Congrats" << endl;
     return 0;
 }
 
-void setup(vector<player*> &players, deck &carddeck, gamestatus &gamestatus)
+void setup(vector<player*> &players, deck &carddeck, gamecontroller &gamecontroller)
 {
     //cout << "How many players do you want (4/6/8)?" << endl;
     //int numplayers;
     //cin >> numplayers;
-    gamestatus.numplayers = 4;
-    carddeck.makedeck(gamestatus.numplayers);
+    gamecontroller.numplayers = 4;
+    carddeck.makedeck(gamecontroller.numplayers);
     players.push_back(new learner());
-    for(int i = 0; i < gamestatus.numplayers - 1; ++i) players.push_back(new trainer());
+    for(int i = 0; i < gamecontroller.numplayers - 1; ++i) players.push_back(new trainer());
 }
 
 void dealcards(vector<player*> &players, const deck &carddeck)
@@ -212,10 +249,10 @@ void dealcards(vector<player*> &players, const deck &carddeck)
     while(index != numplayers*6) players[(i++)%numplayers]->hand.push_back(carddeck.carddeck[index++]);
 }
 
-void makebets(const vector<player*> &players, gamestatus &gamestatus)
+void makebets(const vector<player*> &players, gamecontroller &gamecontroller)
 {//FIX THIS
-    int currentbetter = (gamestatus.whodealt+2) % gamestatus.numplayers;
-    gamestatus.whodealt = (gamestatus.whodealt+1) % gamestatus.numplayers;
-    for(int dealer = currentbetter - 1; currentbetter != dealer && !players[currentbetter]->bet(gamestatus, currentbetter); currentbetter = (currentbetter + 1) % gamestatus.numplayers);
-    if(currentbetter == gamestatus.whodealt) players[currentbetter]->forcebet(gamestatus, currentbetter);
+    int currentbetter = (gamecontroller.whodealt+2) % gamecontroller.numplayers;
+    gamecontroller.whodealt = (gamecontroller.whodealt+1) % gamecontroller.numplayers;
+    for(int dealer = currentbetter - 1; currentbetter != dealer && !players[currentbetter]->bet(gamecontroller, currentbetter); currentbetter = (currentbetter + 1) % gamecontroller.numplayers);
+    if(currentbetter == gamecontroller.whodealt) players[currentbetter]->forcebet(gamecontroller, currentbetter);
 }
